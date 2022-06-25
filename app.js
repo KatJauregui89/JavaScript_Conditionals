93aeb9de02da604cf6fc29d699e1d91245326857
katjauregui89 <katiajauregui@gmail.com>
2022-06-25 12:21:40 -0500

    Else age

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-let random = Math.random();
-if (random < 0.5) {
-    console.log("Your number is less than 0.5")
-}
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("Your number is less than 0.5")
+// }
 
-else {
-    console.log("Your number is equal or greater than 0.5");
-}
-console.log(random);
+// else {
+//     console.log("Your number is equal or greater than 0.5");
+// }
+// console.log(random);
+
+
 // const dayOfWeek = prompt('Enter day:').toLowerCase();
 
 // if (dayOfWeek === "monday") {
@@ -25,13 +27,13 @@ console.log(random);
 // 10 - 65 ADULT $20
 // 65 + SENIOR 
 
-// const age = 35;
-// if (age < 5) {
-//     console.log("FREE ENTREE")
-// } else if (age < 10) {
-//     console.log("You are a child Ticket $10")
-// } else if (age < 65) {
-//     console.log("You are an adult Ticket $20")
-// } else if (age >= 65) {
-//     console.log("You are a Senior Ticket $10")
-// }
+const age = 35;
+if (age < 5) {
+    console.log("FREE ENTREE")
+} else if (age < 10) {
+    console.log("You are a child Ticket $10")
+} else if (age < 65) {
+    console.log("You are an adult Ticket $20")
+} else {
+    console.log("You are a Senior Ticket $10")
+}
