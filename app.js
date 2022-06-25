12d2d5fd9a92a6683da689ca7c69ae139dec62be
katjauregui89 <katiajauregui@gmail.com>
2022-06-25 10:42:40 -0500

    Ages

--- app.js
+++ app.js
@@ -9,12 +9,28 @@
 //     console.log(random);
 // }
 
-const dayOfWeek = "Friday";
+// const dayOfWeek = "Friday";
 
-if (dayOfWeek === "Monday") {
-    console.log("I hate Mondays");
-} else if (dayOfWeek === "Saturday") {
-    console.log("I love Saturdays");
-} else if (dayOfWeek === "Friday") {
-    console.log("I like Fridays after work");
+// if (dayOfWeek === "Monday") {
+//     console.log("I hate Mondays");
+// } else if (dayOfWeek === "Saturday") {
+//     console.log("I love Saturdays");
+// } else if (dayOfWeek === "Friday") {
+//     console.log("I like Fridays after work");
+// }
+
+// 0-5 - FREE
+// 5 - 10 CHILD $10
+// 10 - 65 ADULT $20
+// 65 + SENIOR 
+
+const age = 35;
+if (age < 5) {
+    console.log("FREE ENTREE")
+} else if (age < 10) {
+    console.log("You are a child Ticket $10")
+} else if (age < 65) {
+    console.log("You are an adult Ticket $20")
+} else if (age >= 65) {
+    console.log("You are a Senior Ticket $10")
 }
