078cdbb9344ed39ee7fe0f10b213dd12f5aa4ace
katjauregui89 <katiajauregui@gmail.com>
2022-06-25 10:32:06 -0500

    Days of week

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("is working")
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("Your number is less than 0.5")
+//     console.log(random);
+// }
+
+// else if (random >= 0.5) {
+//     console.log("Your number is equal or greater than 0.5");
+//     console.log(random);
+// }
+
+const dayOfWeek = "Friday";
+
+if (dayOfWeek === "Monday") {
+    console.log("I hate Mondays");
+} else if (dayOfWeek === "Saturday") {
+    console.log("I love Saturdays");
+} else if (dayOfWeek === "Friday") {
+    console.log("I like Fridays after work");
+}
