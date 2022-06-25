362bb6ecd48c8abc37e86343f0cfcae7672a90fd
katjauregui89 <katiajauregui@gmail.com>
2022-06-25 12:20:17 -0500

    Else

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-// let random = Math.random();
-// if (random < 0.5) {
-//     console.log("Your number is less than 0.5")
-//     console.log(random);
-// }
+let random = Math.random();
+if (random < 0.5) {
+    console.log("Your number is less than 0.5")
+}
 
-// else if (random >= 0.5) {
-//     console.log("Your number is equal or greater than 0.5");
-//     console.log(random);
-// }
+else {
+    console.log("Your number is equal or greater than 0.5");
+}
+console.log(random);
+// const dayOfWeek = prompt('Enter day:').toLowerCase();
 
-// const dayOfWeek = "Friday";
-
-// if (dayOfWeek === "Monday") {
+// if (dayOfWeek === "monday") {
 //     console.log("I hate Mondays");
-// } else if (dayOfWeek === "Saturday") {
+// } else if (dayOfWeek === "saturday") {
 //     console.log("I love Saturdays");
-// } else if (dayOfWeek === "Friday") {
+// } else if (dayOfWeek === "friday") {
 //     console.log("I like Fridays after work");
+// } else {
+//     console.log("Meh")
 // }
+
 
 // 0-5 - FREE
 // 5 - 10 CHILD $10
 // 10 - 65 ADULT $20
 // 65 + SENIOR 
 
-const age = 35;
-if (age < 5) {
-    console.log("FREE ENTREE")
-} else if (age < 10) {
-    console.log("You are a child Ticket $10")
-} else if (age < 65) {
-    console.log("You are an adult Ticket $20")
-} else if (age >= 65) {
-    console.log("You are a Senior Ticket $10")
-}
+// const age = 35;
+// if (age < 5) {
+//     console.log("FREE ENTREE")
+// } else if (age < 10) {
+//     console.log("You are a child Ticket $10")
+// } else if (age < 65) {
+//     console.log("You are an adult Ticket $20")
+// } else if (age >= 65) {
+//     console.log("You are a Senior Ticket $10")
+// }
